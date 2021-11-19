62a3f6afb4f4ff34a4e2ce61b4462b63f4eae1d4
Simakin <simakink@gmail.com>
2021-11-19 17:31:57 +0300

    remove ssh and files

--- cpu-checker.sh
+++ cpu-checker.sh
@@ -5,5 +5,5 @@ thread=$3
 
 while [ -f $tmpfile ];
 do
-    /usr/bin/ssh -i /home/ubuntu/.ssh/id_rsa ubuntu@${host} "sleep 1; top -p \$(pidof mysqld) -b -n1"|grep '%CPU' -A1|awk 'NR>1{print $9}' >> $tmpfile
+    sleep 1; top -p \$(pidof mysqld) -b -n1|grep '%CPU' -A1|awk 'NR>1{print $9}' >> $tmpfile
 done
